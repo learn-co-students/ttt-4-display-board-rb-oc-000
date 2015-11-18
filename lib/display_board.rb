9accf718f62b6aad80501141f9b8755eaa4c7fc9
phamducv <phamducv@gmail.com>
2015-11-18 17:23:45 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,4 @@
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,5 +7,19 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]# Define display_board that accepts a board and prints
+board=["X", "X", " ", " ", " ", " ", " ", " ", " "]
+
+
+puts "Whats your name?"
+name=gets.chomp
+puts "Well, #{name}, wanna play tic tac toe today"
+
+display_board(board)
+
+
+
+
+
+
+# Define display_board that accepts a board and prints
 # out the current state.
