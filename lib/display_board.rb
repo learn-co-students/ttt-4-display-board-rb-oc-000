ba5aa0a486715ace1a0b1af88b4971358efb2326
backpackwithjets <matthew.fullen@gmail.com>
2016-05-23 17:12:22 +0000

    nitrous-backup at Mon May 23 17:12:22 UTC 2016

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,60 +1,18 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+board = [" ", "O", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts ["   | X |   "]
   puts ["-----------"]
   puts ["   |   |   "]
   puts ["-----------"]
   puts ["   |   |   "]
-
-  puts [" O |   |   "]
+  
+  puts [" #board{1}  |   |   "]
   puts ["-----------"]
   puts ["   |   |   "]
   puts ["-----------"]
   puts ["   |   |   "]
 
-  puts [" X | X | X "]
-  puts ["-----------"]
-  puts ["   |   |   "]
-  puts ["-----------"]
-  puts ["   |   |   "]
 
-  puts ["   |   |   "]
-  puts ["-----------"]
-  puts ["   |   |   "]
-  puts ["-----------"]
-  puts [" O | O | O "]
-
-  puts [" X |   |   "]
-  puts ["-----------"]
-  puts ["   | X |   "]
-  puts ["-----------"]
-  puts ["   |   | X "]
-
-  puts ["   |   | O "]
-  puts ["-----------"]
-  puts ["   | O |   "]
-  puts ["-----------"]
-  puts [" O |   |   "]
-
-  puts [" X | O | O "]
-  puts ["-----------"]
-  puts ["   |   |   "]
-  puts ["-----------"]
-  puts ["   |   |   "]
-
-  puts [" X | O | X "]
-  puts ["-----------"]
-  puts ["   |   |   "]
-  puts ["-----------"]
-  puts ["   |   |   "]
-
-  puts [" O | #{board[0]} | X "]
-  puts ["-----------"]
-  puts [" O | #{board[0]} | O "]
-  puts ["-----------"]
-  puts ["   |   |   "]
-
-  puts #{board}
 end
